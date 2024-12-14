4e8fcc63b83e50f163ed0983f92280051e215964
Gurdeep2121 <gurdeep.singh60@qmail.cuny.edu>
2024-12-14 00:52:07 +0000

    final final

--- app/final/page.tsx
+++ app/final/page.tsx
@@ -7,99 +7,105 @@ export default function Page() {
     <h3 className="mx-5 my-2 font-bold underline text-orange-600 text-2xl">Contact Information:</h3>
     <form>
       <label htmlFor="product" className="mx-5 my-2 font-bold text-blue-800">First Name: *</label>
-      <input type="text" name="product" id="product" className="m-5 text-black bg-gray-200"/>
+      <input type="text" name="First" id="product" className="m-5 text-black bg-gray-200"/>
       <br />
       <label htmlFor="product" className="mx-5 my-2 font-bold text-blue-800">Middle Name:</label>
-      <input type="text" name="product" id="product" className="m-5 text-black bg-gray-200"/>
+      <input type="text" name="Middle" id="product" className="m-5 text-black bg-gray-200"/>
       <br />
       <label htmlFor="product" className="mx-5 my-2 font-bold text-blue-800">Last Name: *</label>
-      <input type="text" name="product" id="product" className="m-5 text-black bg-gray-200"/>
+      <input type="text" name="Last" id="product" className="m-5 text-black bg-gray-200"/>
       <br />
       <label htmlFor="product" className="mx-5 my-2 font-bold text-blue-800">Phone #: *</label>
-      <input type="text" name="product" id="product" className="m-5 text-black bg-gray-200"/>
+      <input type="text" name="Phone" id="product" className="m-5 text-black bg-gray-200"/>
       <br />
       <label htmlFor="product" className="mx-5 my-2 font-bold text-blue-800">Email: *</label>
-      <input type="text" name="product" id="product" className="m-5 text-black bg-gray-200"/>
+      <input type="text" name="email" id="product" className="m-5 text-black bg-gray-200"/>
       <br />
 
       <label htmlFor="game" className="mx-5 my-2 font-bold underline text-orange-600 text-2xl">Choose a game:</label>
-      <select name="game" id="game">
+      <select name="game" id="game" className="border-2 border-orange-600 rounded-lg">
       
       <option value="Christmas">Christmas Day Game vs Spurs</option>
       <option value="New Year">New Year Day Game vs Jazz</option>
       </select>
 
       <br />
+      <br />
       <h4 className="mx-5 my-1 font-bold text-blue-800">Question 1: Who is the only Knick to have ever won a regular season MVP? </h4> 
       <div className="ml-5 mt-5">
-      <input type="radio" id="Wilis Reed" name="fav_language" value="Willis Reed" />
+      <input type="radio" id="Wilis Reed" name="MVP" value="Willis Reed" />
       <label htmlFor="Willis Reed"> Willis Reed</label><br />
-      <input type="radio" id="css" name="fav_language" value="CSS" />
+      <input type="radio" id="css" name="MVP" value="Patrick Ewing" />
       <label htmlFor="css"> Patrick Ewing</label><br />
-      <input type="radio" id="javascript" name="fav_language" value="JavaScript" />
+      <input type="radio" id="javascript" name="MVP" value="Walt Frazier" />
       <label htmlFor="javascript"> Walt Frazier</label><br />
-      <input type="radio" id="css" name="fav_language" value="CSS" />
+      <input type="radio" id="css" name="MVP" value="Carmelo Anthony" />
       <label htmlFor="css"> Carmelo Anthony</label>
       </div>
     
       <br />
       <h4 className="mx-5 my-1 font-bold text-blue-800">Question 2: Which current Knick has won the NBA three point competition?  </h4> 
       <div className="ml-5 mt-5">
-      <input type="radio" id="Wilis Reed" name="fav_language" value="Willis Reed" />
+      <input type="radio" id="Wilis Reed" name="3PT" value="Jalen Brunson" />
       <label htmlFor="Willis Reed"> Jalen Brunson</label><br />
-      <input type="radio" id="css" name="fav_language" value="CSS" />
+      <input type="radio" id="css" name="3PT" value="OG Anunoby" />
       <label htmlFor="css"> OG Anunoby</label><br />
-      <input type="radio" id="javascript" name="fav_language" value="JavaScript" />
+      <input type="radio" id="javascript" name="3PT" value="Mikal Bridges" />
       <label htmlFor="javascript"> Mikal Bridges</label><br />
-      <input type="radio" id="css" name="fav_language" value="CSS" />
+      <input type="radio" id="css" name="3PT" value="Karl-Anthony Towns" />
       <label htmlFor="css"> Karl-Anthony Towns</label>
       </div>
       <br />
 
       <h4 className="mx-5 my-1 font-bold text-blue-800">Question 3: When referencing DFG% as the metric to assess defensive performance, which current Knicks player is statistically the best defensive player for the 2024-25 season? </h4> 
       <div className="ml-5 mt-5">
-      <input type="radio" id="html" name="fav_language" value="html" />
+      <input type="radio" id="html" name="DFG" value="Karl Anthony-Towns" />
       <label htmlFor="Willis Reed"> Karl Anthony-Towns</label><br />
-      <input type="radio" id="css" name="fav_language" value="CSS" />
+      <input type="radio" id="css" name="DFG" value="OG Anunoby" />
       <label htmlFor="css"> OG Anunoby</label><br />
-      <input type="radio" id="javascript" name="fav_language" value="JavaScript" />
+      <input type="radio" id="javascript" name="DFG" value="Jericho Sims" />
       <label htmlFor="javascript"> Jericho Sims</label><br />
-      <input type="radio" id="css" name="fav_language" value="CSS" />
+      <input type="radio" id="css" name="DFG" value="Josh Hart" />
       <label htmlFor="css"> Josh Hart</label>
       </div>
       <br />
       <h4 className="mx-5 my-1 font-bold text-blue-800">Question 4: Select ALL the players that won the NBA slam dunk contest while playing for the New York Knicks: </h4> 
       <div className="ml-5 mt-5">
-      <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike" />
+      <input type="checkbox" id="vehicle1" name="Dunk" value="James White" />
       <label htmlFor="vehicle1"> James White</label><br />
-      <input type="checkbox" id="vehicle2" name="vehicle2" value="Car" />
+      <input type="checkbox" id="vehicle2" name="Dunk" value="Kenny Walker" />
       <label htmlFor="vehicle2"> Kenny Walker</label><br />
-      <input type="checkbox" id="vehicle2" name="vehicle2" value="Car" />
+      <input type="checkbox" id="vehicle2" name="Dunk" value="Nate Robinson" />
       <label htmlFor="vehicle2"> Nate Robinson</label><br />
-      <input type="checkbox" id="vehicle2" name="vehicle2" value="Car" />
+      <input type="checkbox" id="vehicle2" name="Dunk" value="Obi Toppin" />
       <label htmlFor="vehicle2"> Obi Toppin</label><br />
-      <input type="checkbox" id="vehicle2" name="vehicle2" value="Car" />
+      <input type="checkbox" id="vehicle2" name="Dunk" value="Jericho Sims" />
       <label htmlFor="vehicle2"> Jericho Sims</label><br />
       </div>
       <br />
 
       <h4 className="mx-5 my-1 font-bold text-blue-800">Question 5: Select ALL the players in the Knicks starting lineup that were on the roster during the 2023-24 season: </h4> 
       <div className="ml-5 mt-5">
-      <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike" />
+      <input type="checkbox" id="vehicle1" name="starter" value="Jalen Brunson" />
       <label htmlFor="vehicle1"> Jalen Brunson</label><br />
-      <input type="checkbox" id="vehicle2" name="vehicle2" value="Car" />
+      <input type="checkbox" id="vehicle2" name="starter" value="Mikal Bridges" />
       <label htmlFor="vehicle2"> Mikal Bridges</label><br />
-      <input type="checkbox" id="vehicle2" name="vehicle2" value="Car" />
+      <input type="checkbox" id="vehicle2" name="starter" value="Josh Hart" />
       <label htmlFor="vehicle2"> Josh Hart</label><br />
-      <input type="checkbox" id="vehicle2" name="vehicle2" value="Car" />
+      <input type="checkbox" id="vehicle2" name="starter" value="OG Anunoby" />
       <label htmlFor="vehicle2"> OG Anunoby</label><br />
-      <input type="checkbox" id="vehicle2" name="vehicle2" value="Car" />
+      <input type="checkbox" id="vehicle2" name="starter" value="Karl-Anthony Towns" />
       <label htmlFor="vehicle2"> Karl-Anthony Towns</label><br />
       </div>
       <br />
 
-      <button className="ml-10 focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" type="submit">Submit Form</button>
-    </form>
-    </>
-  );
+      <br />
+      <button type="submit" className="bg-orange-500 text-white font-bold px-5 py-2.5 ml-5 me-20 mb-2 rounded hover:bg-orange-700 border-2 border-orange-600 hover:border-orange-700">
+  Submit Form
+</button>
+
+</form>
+</>
+);
 }
+
